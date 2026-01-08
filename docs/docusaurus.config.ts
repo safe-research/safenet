466f79f55266f26bd03332b58e17b309e8cd2dd6
Tobias Schubotz <1380529+tschubotz@users.noreply.github.com>
2026-01-08 14:14:17 +0100

    Additions for gh pages

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -16,11 +16,13 @@ const config: Config = {
   },
 
   // Set the production url of your site here 
-  // TODO: Update once known
-  url: 'https://your-docusaurus-site.example.com',
+  // TODO: Update once moved to actual prod
+  url: 'https://safe.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/shieldnet/',
+  trailingSlash: false,
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
