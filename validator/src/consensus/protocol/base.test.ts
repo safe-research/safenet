5315071fc52bf64ab5f552837f7b63a51ed985d9
Nicholas Rodrigues Lordello <n@lordello.net>
2026-03-13 11:19:45 +0100

    Remove In-Memory Storage (#281)
    In order to reduce some code-paths, I removed all of the `InMemory*`
    storage implementations. The old behaviour can be replicated by using
    the `:memory:` Sqlite database.
    
    There was one place where we used an in-memory queue for transient data,
    but I changed the implementation to use arrays with `push` and `shift`.
    I renamed the queue methods, as I thought they were easy to confuse with
    `Stack` operations (in fact, if you replaced a `Queue` value with an
    array, you would get the same `push` and `pop` methods, but with FILO
    instead of FIFO scemantics).

--- validator/src/consensus/protocol/base.test.ts
+++ validator/src/consensus/protocol/base.test.ts
@@ -1,9 +1,9 @@
+import Sqlite3 from "better-sqlite3";
 import { zeroHash } from "viem";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { testLogger } from "../../__tests__/config.js";
 import { TEST_ACTIONS, TestProtocol } from "../../__tests__/data/protocol.js";
-import { InMemoryQueue } from "../../utils/queue.js";
-import type { ActionWithTimeout } from "./types.js";
+import { SqliteActionQueue } from "./sqlite.js";
 
 describe("BaseProtocol", () => {
 	beforeEach(() => {
@@ -16,10 +16,10 @@ describe("BaseProtocol", () => {
 
 	it("should retry actions that errored", async () => {
 		const timeoutSpy = vi.spyOn(global, "setTimeout");
-		const queue = new InMemoryQueue<ActionWithTimeout>();
-		const pushSpy = vi.spyOn(queue, "push");
+		const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+		const enqueueSpy = vi.spyOn(queue, "enqueue");
 		const peekSpy = vi.spyOn(queue, "peek");
-		const popSpy = vi.spyOn(queue, "pop");
+		const dequeueSpy = vi.spyOn(queue, "dequeue");
 		const protocol = new TestProtocol(queue, testLogger);
 		const protocolSpy = vi.spyOn(protocol, "requestSignature");
 		expect(queue.peek()).toBeUndefined();
@@ -30,8 +30,8 @@ describe("BaseProtocol", () => {
 			validUntil: Date.now() + 10000,
 		};
 		expect(queue.peek()).toStrictEqual(actionWithTimeout);
-		expect(pushSpy).toBeCalledTimes(1);
-		expect(pushSpy).toBeCalledWith(actionWithTimeout);
+		expect(enqueueSpy).toBeCalledTimes(1);
+		expect(enqueueSpy).toBeCalledWith(actionWithTimeout);
 		// Called 3 times: 2 times in the test, 1 time in the implementation
 		expect(peekSpy).toBeCalledTimes(3);
 		// Check if retry was scheduled via setTimeout
@@ -44,9 +44,9 @@ describe("BaseProtocol", () => {
 		vi.advanceTimersByTime(1000);
 		// Wait for retry to be successful
 		await vi.waitFor(() => {
-			expect(popSpy).toHaveBeenCalled();
+			expect(dequeueSpy).toHaveBeenCalled();
 		});
-		expect(popSpy).toBeCalledTimes(1);
+		expect(dequeueSpy).toBeCalledTimes(1);
 		// Called 3 times before, 1 additional time by retry, 1 time after retry
 		expect(peekSpy).toBeCalledTimes(5);
 		expect(protocolSpy).toBeCalledTimes(2);
@@ -57,10 +57,10 @@ describe("BaseProtocol", () => {
 
 	it("should drop actions after timeout", async () => {
 		const timeoutSpy = vi.spyOn(global, "setTimeout");
-		const queue = new InMemoryQueue<ActionWithTimeout>();
-		const pushSpy = vi.spyOn(queue, "push");
+		const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+		const enqueueSpy = vi.spyOn(queue, "enqueue");
 		const peekSpy = vi.spyOn(queue, "peek");
-		const popSpy = vi.spyOn(queue, "pop");
+		const dequeueSpy = vi.spyOn(queue, "dequeue");
 		const protocol = new TestProtocol(queue, testLogger);
 		const protocolSpy = vi.spyOn(protocol, "requestSignature");
 		expect(queue.peek()).toBeUndefined();
@@ -71,8 +71,8 @@ describe("BaseProtocol", () => {
 			validUntil: Date.now(),
 		};
 		expect(queue.peek()).toStrictEqual(actionWithTimeout);
-		expect(pushSpy).toBeCalledTimes(1);
-		expect(pushSpy).toBeCalledWith(actionWithTimeout);
+		expect(enqueueSpy).toBeCalledTimes(1);
+		expect(enqueueSpy).toBeCalledWith(actionWithTimeout);
 		// Called 3 times: 2 times in the test, 1 time in the implementation
 		expect(peekSpy).toBeCalledTimes(3);
 		// Check if retry was scheduled via setTimeout
@@ -86,9 +86,9 @@ describe("BaseProtocol", () => {
 		vi.advanceTimersByTime(1000);
 		// Wait for retry to be successful
 		await vi.waitFor(() => {
-			expect(popSpy).toHaveBeenCalled();
+			expect(dequeueSpy).toHaveBeenCalled();
 		});
-		expect(popSpy).toBeCalledTimes(1);
+		expect(dequeueSpy).toBeCalledTimes(1);
 		// No additional function should be triggered as the action was dropped
 		expect(protocolSpy).toBeCalledTimes(1);
 		// Called 3 times before, 1 additional time by retry, 1 time after retry
@@ -99,10 +99,10 @@ describe("BaseProtocol", () => {
 
 	it("should not trigger immediate processing if retry is scheduled", async () => {
 		const timeoutSpy = vi.spyOn(global, "setTimeout");
-		const queue = new InMemoryQueue<ActionWithTimeout>();
-		const pushSpy = vi.spyOn(queue, "push");
+		const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+		const enqueueSpy = vi.spyOn(queue, "enqueue");
 		const peekSpy = vi.spyOn(queue, "peek");
-		const popSpy = vi.spyOn(queue, "pop");
+		const dequeueSpy = vi.spyOn(queue, "dequeue");
 		const protocol = new TestProtocol(queue, testLogger);
 		const protocolSpy = vi.spyOn(protocol, "requestSignature");
 		expect(queue.peek()).toBeUndefined();
@@ -113,8 +113,8 @@ describe("BaseProtocol", () => {
 			validUntil: Date.now(),
 		};
 		expect(queue.peek()).toStrictEqual(actionWithTimeout);
-		expect(pushSpy).toBeCalledTimes(1);
-		expect(pushSpy).toBeCalledWith(actionWithTimeout);
+		expect(enqueueSpy).toBeCalledTimes(1);
+		expect(enqueueSpy).toBeCalledWith(actionWithTimeout);
 		// Called 3 times: 2 times in the test, 1 time in the implementation
 		expect(peekSpy).toBeCalledTimes(3);
 		// Check if retry was scheduled via setTimeout
@@ -132,10 +132,10 @@ describe("BaseProtocol", () => {
 		vi.advanceTimersByTime(1000);
 		// Wait for retry to be successful
 		await vi.waitFor(() => {
-			expect(popSpy).toHaveBeenCalled();
+			expect(dequeueSpy).toHaveBeenCalled();
 		});
 		// Two transactions were queued
-		expect(popSpy).toBeCalledTimes(2);
+		expect(dequeueSpy).toBeCalledTimes(2);
 		// No additional function should be triggered as the action was dropped
 		expect(protocolSpy).toBeCalledTimes(1);
 		// Called 3 times before, 1 additional time by retry, 1 time after retry, 1 time for additional action
@@ -146,10 +146,10 @@ describe("BaseProtocol", () => {
 
 	it("should increase delay on error retry until maximum is reached", async () => {
 		const timeoutSpy = vi.spyOn(global, "setTimeout");
-		const queue = new InMemoryQueue<ActionWithTimeout>();
-		const pushSpy = vi.spyOn(queue, "push");
+		const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+		const enqueueSpy = vi.spyOn(queue, "enqueue");
 		const peekSpy = vi.spyOn(queue, "peek");
-		const popSpy = vi.spyOn(queue, "pop");
+		const dequeueSpy = vi.spyOn(queue, "dequeue");
 		const protocol = new TestProtocol(queue, testLogger);
 		const protocolSpy = vi.spyOn(protocol, "requestSignature");
 		expect(queue.peek()).toBeUndefined();
@@ -160,8 +160,8 @@ describe("BaseProtocol", () => {
 			validUntil: Date.now() + 25000,
 		};
 		expect(queue.peek()).toStrictEqual(actionWithTimeout);
-		expect(pushSpy).toBeCalledTimes(1);
-		expect(pushSpy).toBeCalledWith(actionWithTimeout);
+		expect(enqueueSpy).toBeCalledTimes(1);
+		expect(enqueueSpy).toBeCalledWith(actionWithTimeout);
 		// Called 3 times: 2 times in the test, 1 time in the implementation
 		expect(peekSpy).toBeCalledTimes(3);
 		// Check if retry was scheduled via setTimeout
@@ -179,17 +179,17 @@ describe("BaseProtocol", () => {
 		// Should not increase as retry is scheduled
 		expect(peekSpy).toBeCalledTimes(11);
 		expect(protocolSpy).toBeCalledTimes(7);
-		expect(popSpy).toBeCalledTimes(1);
+		expect(dequeueSpy).toBeCalledTimes(1);
 		// Queue should be empty now
 		expect(queue.peek()).toBeUndefined();
 	});
 
 	it("should reset delay after successfull action submission", async () => {
 		const timeoutSpy = vi.spyOn(global, "setTimeout");
-		const queue = new InMemoryQueue<ActionWithTimeout>();
-		const pushSpy = vi.spyOn(queue, "push");
+		const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+		const enqueueSpy = vi.spyOn(queue, "enqueue");
 		const peekSpy = vi.spyOn(queue, "peek");
-		const popSpy = vi.spyOn(queue, "pop");
+		const dequeueSpy = vi.spyOn(queue, "dequeue");
 		const protocol = new TestProtocol(queue, testLogger);
 		const protocolSpy = vi.spyOn(protocol, "requestSignature");
 		protocolSpy.mockRejectedValueOnce("Test Error");
@@ -204,8 +204,8 @@ describe("BaseProtocol", () => {
 			validUntil: Date.now() + 50000,
 		};
 		expect(queue.peek()).toStrictEqual(actionWithTimeout);
-		expect(pushSpy).toBeCalledTimes(1);
-		expect(pushSpy).toBeCalledWith(actionWithTimeout);
+		expect(enqueueSpy).toBeCalledTimes(1);
+		expect(enqueueSpy).toBeCalledWith(actionWithTimeout);
 		// Called 3 times: 2 times in the test, 1 time in the implementation
 		expect(peekSpy).toBeCalledTimes(3);
 		// Check if retry was scheduled via setTimeout
@@ -218,9 +218,9 @@ describe("BaseProtocol", () => {
 		}
 		vi.advanceTimersByTime(5000);
 		await vi.waitFor(() => {
-			expect(popSpy).toHaveBeenCalled();
+			expect(dequeueSpy).toHaveBeenCalled();
 		});
-		expect(popSpy).toBeCalledTimes(1);
+		expect(dequeueSpy).toBeCalledTimes(1);
 		// Queue should be empty now
 		expect(queue.peek()).toBeUndefined();
 		expect(timeoutSpy).toBeCalledTimes(2);
@@ -235,10 +235,10 @@ describe("BaseProtocol", () => {
 
 	it("should reset delay after action timed out", async () => {
 		const timeoutSpy = vi.spyOn(global, "setTimeout");
-		const queue = new InMemoryQueue<ActionWithTimeout>();
-		const pushSpy = vi.spyOn(queue, "push");
+		const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+		const enqueueSpy = vi.spyOn(queue, "enqueue");
 		const peekSpy = vi.spyOn(queue, "peek");
-		const popSpy = vi.spyOn(queue, "pop");
+		const dequeueSpy = vi.spyOn(queue, "dequeue");
 		const protocol = new TestProtocol(queue, testLogger);
 		expect(queue.peek()).toBeUndefined();
 		const action = TEST_ACTIONS[0][0];
@@ -248,8 +248,8 @@ describe("BaseProtocol", () => {
 			validUntil: Date.now() + 3000,
 		};
 		expect(queue.peek()).toStrictEqual(actionWithTimeout);
-		expect(pushSpy).toBeCalledTimes(1);
-		expect(pushSpy).toBeCalledWith(actionWithTimeout);
+		expect(enqueueSpy).toBeCalledTimes(1);
+		expect(enqueueSpy).toBeCalledWith(actionWithTimeout);
 		// Called 3 times: 2 times in the test, 1 time in the implementation
 		expect(peekSpy).toBeCalledTimes(3);
 		// Check if retry was scheduled via setTimeout
@@ -262,9 +262,9 @@ describe("BaseProtocol", () => {
 		}
 		vi.advanceTimersByTime(5000);
 		await vi.waitFor(() => {
-			expect(popSpy).toHaveBeenCalled();
+			expect(dequeueSpy).toHaveBeenCalled();
 		});
-		expect(popSpy).toBeCalledTimes(1);
+		expect(dequeueSpy).toBeCalledTimes(1);
 		// Queue should be empty now
 		expect(queue.peek()).toBeUndefined();
 		expect(timeoutSpy).toBeCalledTimes(2);
@@ -281,8 +281,8 @@ describe("BaseProtocol", () => {
 
 	it("should be able to enqueue multiple actions", async () => {
 		const timeoutSpy = vi.spyOn(global, "setTimeout");
-		const queue = new InMemoryQueue<ActionWithTimeout>();
-		const popSpy = vi.spyOn(queue, "pop");
+		const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+		const dequeueSpy = vi.spyOn(queue, "dequeue");
 		const protocol = new TestProtocol(queue, testLogger);
 		const protocolSpy = vi.spyOn(protocol, "requestSignature");
 		// Do not resolve promise
@@ -292,14 +292,14 @@ describe("BaseProtocol", () => {
 			protocol.process(action, 0);
 		}
 		// No action should be completed
-		expect(popSpy).toBeCalledTimes(0);
+		expect(dequeueSpy).toBeCalledTimes(0);
 		expect(timeoutSpy).toBeCalledTimes(0);
 		for (const [action] of TEST_ACTIONS) {
 			const actionWithTimeout = {
 				...action,
 				validUntil,
 			};
-			expect(queue.pop()).toStrictEqual(actionWithTimeout);
+			expect(queue.dequeue()).toStrictEqual(actionWithTimeout);
 		}
 	});
 
@@ -313,10 +313,10 @@ describe("BaseProtocol", () => {
 		}),
 	)("for $description", ({ action, functionName }) => {
 		it(`should call ${functionName}`, async () => {
-			const queue = new InMemoryQueue<ActionWithTimeout>();
-			const pushSpy = vi.spyOn(queue, "push");
+			const queue = new SqliteActionQueue(new Sqlite3(":memory:"));
+			const enqueueSpy = vi.spyOn(queue, "enqueue");
 			const peekSpy = vi.spyOn(queue, "peek");
-			const popSpy = vi.spyOn(queue, "pop");
+			const dequeueSpy = vi.spyOn(queue, "dequeue");
 			const protocol = new TestProtocol(queue, testLogger);
 			const protocolSpy = vi.spyOn(protocol, functionName);
 			protocolSpy.mockResolvedValueOnce(zeroHash);
@@ -327,11 +327,11 @@ describe("BaseProtocol", () => {
 			};
 			// Wait for the pop that is triggered after successful processing
 			await vi.waitFor(() => {
-				expect(popSpy).toHaveBeenCalled();
+				expect(dequeueSpy).toHaveBeenCalled();
 			});
-			expect(popSpy).toBeCalledTimes(1);
-			expect(pushSpy).toBeCalledTimes(1);
-			expect(pushSpy).toBeCalledWith(actionWithTimeout);
+			expect(dequeueSpy).toBeCalledTimes(1);
+			expect(enqueueSpy).toBeCalledTimes(1);
+			expect(enqueueSpy).toBeCalledWith(actionWithTimeout);
 			expect(protocolSpy).toBeCalledTimes(1);
 			expect(protocolSpy).toBeCalledWith(actionWithTimeout);
 			// Called 1 to process, 1 time after process
--- validator/src/machine/transitions/queue.test.ts
+++ validator/src/machine/transitions/queue.test.ts
@@ -9,11 +9,11 @@ describe("SqliteActionQueue", () => {
 
 		expect(storage.peek()).toBeUndefined();
 		for (const [, action] of TEST_EVENTS) {
-			storage.push(action);
+			storage.enqueue(action);
 		}
 		for (const [, action] of TEST_EVENTS) {
 			expect(storage.peek()).toStrictEqual(action);
-			expect(storage.pop()).toStrictEqual(action);
+			expect(storage.dequeue()).toStrictEqual(action);
 		}
 		expect(storage.peek()).toBeUndefined();
 	});
