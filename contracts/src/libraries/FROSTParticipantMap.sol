d550c2b8c1dba28bb99f0d01c53a63f3cfe33bf1
Nicholas Rodrigues Lordello <n@lordello.net>
2026-03-13 13:43:31 +0100

    Use Derived Participant IDs (#279)
    This PR changes the FROST coordinator contract to use derived FROST
    identifiers for the participants (following the reference Rust
    `frost-secp256k1` implementation) instead of arbitrary ones.
    
    The reason for this, is it allows the `FROSTCoordinator` API to be
    completely agnostic to the FROST identifiers, making it an internal
    implementation detail. This simplifies a lot of things for us:
    
    - Events now directly emit with the `participant` address and not with
    `identifier`s, making it much easier to know which validator did what.
    This will greatly simplify the rewards scripts, in that they will no
    longer need to index `KeyGen` events in order to join signature
    participation `identifier`s with `KeyGenCommitment`s in order to
    determine the participant address for a given identifier.
    - The FROST identifiers are now stable for validators, meaning validator
    rotation will not change the internal identifiers. This allows us to
    remove some of the smells that we needed in order to map FROST
    identifiers to validator info from the `explorer`.
    
    Note that this PR _just_ changes the contracts (so integration tests
    *are expected to fail*). It was a big enough change, that I didn't want
    to do it all at once (which would have made it much harder to review).

--- contracts/src/libraries/FROSTParticipantMap.sol
+++ contracts/src/libraries/FROSTParticipantMap.sol
@@ -1,22 +1,32 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.30;
 
-import {Hashes} from "@oz/utils/cryptography/Hashes.sol";
 import {MerkleProof} from "@oz/utils/cryptography/MerkleProof.sol";
-import {FROST} from "@/libraries/FROST.sol";
 import {Secp256k1} from "@/libraries/Secp256k1.sol";
 
 /**
  * @title FROST Participant Map
- * @notice A mapping of FROST participants to their identifiers and public verification shares.
+ * @notice A mapping of FROST participants to their DKG state.
  */
 library FROSTParticipantMap {
-    using FROST for FROST.Identifier;
     using Secp256k1 for Secp256k1.Point;
 
     // ============================================================
     // STRUCTS AND ENUMS
     // ============================================================
+
+    /**
+     * @notice The status of a participant.
+     * @custom:enumValue NONE The participant is not registered.
+     * @custom:enumValue REGISTERED The participant is registered and provided a Merkle proof that it belongs to a FROST
+     *                   group.
+     * @custom:enumValue CONFIRMED The participant has confirmed the key generation ceremony and is ready to sign.
+     */
+    enum ParticipantStatus {
+        NONE,
+        REGISTERED,
+        CONFIRMED
+    }
 
     /**
      * @notice The status of a complaint between participants.
@@ -32,35 +42,31 @@ library FROSTParticipantMap {
 
     /**
      * @notice State tracking for a single participant during DKG.
-     * @custom:param confirmed Whether the participant has successfully confirmed their participation in the key
-     *               generation, locking them out of future complaints.
+     * @custom:param status The current status of a participant.
      * @custom:param complaints The number of unresolved complaints this participant has filed against others. A
      *               participant cannot finalize DKG with a non-zero count.
      * @custom:param accusations The number of unresolved accusations filed against this participant by others.
-     * @custom:param statuses Mapping to track the status of complaints made by this participant against specific
-     *               other participants.
      * @dev This struct tracks a participant's progress and status within the DKG ceremony, particularly for the
      *      complaint and resolution process.
      */
     struct ParticipantState {
-        bool confirmed;
-        uint64 complaints;
-        uint64 accusations;
-        mapping(FROST.Identifier accused => ComplaintStatus) statuses;
+        ParticipantStatus status;
+        uint16 complaints;
+        uint16 accusations;
     }
 
     /**
      * @notice The main storage struct for tracking participants.
      * @custom:param root The Merkle root for participant verification.
-     * @custom:param identifiers Mapping from participant address to their identifier.
-     * @custom:param keys Mapping from identifier to public verification share.
-     * @custom:param states Mapping from identifier to participant state.
+     * @custom:param states Mapping from participant address to their state.
+     * @custom:param keys Mapping from participant address to public verification share.
+     * @custom:param complaints Mapping from plaintiff and accused to the status of a complaint.
      */
     struct T {
         bytes32 root;
-        mapping(address participant => FROST.Identifier) identifiers;
-        mapping(FROST.Identifier => Secp256k1.Point) keys;
-        mapping(FROST.Identifier => ParticipantState) states;
+        mapping(address participant => ParticipantState) states;
+        mapping(address participant => Secp256k1.Point) keys;
+        mapping(address plaintiff => mapping(address accused => ComplaintStatus)) complaints;
     }
 
     // ============================================================
@@ -78,12 +84,12 @@ library FROSTParticipantMap {
     error AlreadyInitialized();
 
     /**
-     * @notice Thrown when a participant is already registered.
+     * @notice Thrown when a participant is not valid.
      */
-    error AlreadyRegistered();
+    error InvalidParticipant();
 
     /**
-     * @notice Thrown when an address is not a participant.
+     * @notice Thrown when the Merkle proof of participation is invalid.
      */
     error NotParticipating();
 
@@ -95,27 +101,17 @@ library FROSTParticipantMap {
     /**
      * @notice Thrown when a complaint has already been submitted.
      */
-    error ComplaintAlreadySubmitted();
-
-    /**
-     * @notice Thrown when the accused identifier is invalid.
-     */
-    error InvalidAccused();
-
-    /**
-     * @notice Thrown when a participant has already finalized.
-     */
-    error ParticipantAlreadyFinalized();
+    error AlreadyComplained();
 
     /**
      * @notice Thrown when responding to a non-existent complaint.
      */
-    error ComplaintNotSubmitted();
+    error NotComplaining();
 
     /**
      * @notice Thrown when an unresponded complaint exists.
      */
-    error UnrespondedComplaintExists();
+    error UnrespondedComplaints();
 
     // ============================================================
     // INTERNAL FUNCTIONS
@@ -144,20 +140,16 @@ library FROSTParticipantMap {
     /**
      * @notice Registers a participant to the merkle tree.
      * @param self The storage struct.
-     * @param identifier The participant's FROST identifier.
      * @param participant The participant's address.
      * @param poap The Merkle proof of participation.
      */
-    function register(T storage self, FROST.Identifier identifier, address participant, bytes32[] calldata poap)
-        internal
-    {
-        identifier.requireValidIdentifier();
-        require(FROST.Identifier.unwrap(self.identifiers[participant]) == 0, AlreadyRegistered());
-        bytes32 leaf = Hashes.efficientKeccak256(
-            bytes32(FROST.Identifier.unwrap(identifier)), bytes32(uint256(uint160(participant)))
-        );
+    function register(T storage self, address participant, bytes32[] calldata poap) internal {
+        ParticipantState memory state = self.states[participant];
+        require(state.status == ParticipantStatus.NONE, InvalidParticipant());
+        bytes32 leaf = bytes32(uint256(uint160(participant)));
         require(MerkleProof.verifyCalldata(poap, self.root, leaf), NotParticipating());
-        self.identifiers[participant] = identifier;
+        state.status = ParticipantStatus.REGISTERED;
+        self.states[participant] = state;
     }
 
     /**
@@ -165,15 +157,11 @@ library FROSTParticipantMap {
      * @param self The storage struct.
      * @param participant The participant's address.
      * @param y The participant's public verification share.
-     * @return identifier The participant's FROST identifier.
      */
-    function set(T storage self, address participant, Secp256k1.Point memory y)
-        internal
-        returns (FROST.Identifier identifier)
-    {
+    function set(T storage self, address participant, Secp256k1.Point memory y) internal {
         y.requireNonZero();
-        identifier = identifierOf(self, participant);
-        Secp256k1.Point storage key = self.keys[identifier];
+        require(self.states[participant].status == ParticipantStatus.REGISTERED, InvalidParticipant());
+        Secp256k1.Point storage key = self.keys[participant];
         require(key.x | key.y == 0, AlreadySet());
         key.x = y.x;
         key.y = y.y;
@@ -182,39 +170,40 @@ library FROSTParticipantMap {
     /**
      * @notice Submits a complaint from a plaintiff against an accused participant.
      * @param self The storage struct.
-     * @param plaintiff The plaintiff's FROST identifier.
-     * @param accused The accused's FROST identifier.
+     * @param plaintiff The plaintiff's address.
+     * @param accused The accused's address.
      * @return totalAccusations The total number of unresolved accusations against the accused.
      * @dev This function is a key part of the DKG's security. If a participant detects that another participant is
      *      misbehaving (e.g., by providing an invalid secret share), they can file a public complaint on-chain. A
      *      participant cannot file the same complaint twice or complain after they have already confirmed their own
      *      DKG completion.
      */
-    function complain(T storage self, FROST.Identifier plaintiff, FROST.Identifier accused)
-        internal
-        returns (uint64 totalAccusations)
-    {
-        require(self.states[plaintiff].statuses[accused] == ComplaintStatus.NONE, ComplaintAlreadySubmitted());
-        require(!self.states[plaintiff].confirmed, ParticipantAlreadyFinalized());
-        require(isParticipating(self, accused), InvalidAccused());
-        require(!FROST.identifierEq(plaintiff, accused), InvalidAccused());
-        self.states[plaintiff].statuses[accused] = ComplaintStatus.SUBMITTED;
-        self.states[plaintiff].complaints++;
-        totalAccusations = ++self.states[accused].accusations;
+    function complain(T storage self, address plaintiff, address accused) internal returns (uint16 totalAccusations) {
+        require(plaintiff != accused, InvalidParticipant());
+        require(self.complaints[plaintiff][accused] == ComplaintStatus.NONE, AlreadyComplained());
+        ParticipantState memory plaintiffState = self.states[plaintiff];
+        require(plaintiffState.status == ParticipantStatus.REGISTERED, InvalidParticipant());
+        ParticipantState memory accusedState = self.states[accused];
+        require(accusedState.status != ParticipantStatus.NONE, InvalidParticipant());
+        self.complaints[plaintiff][accused] = ComplaintStatus.SUBMITTED;
+        plaintiffState.complaints++;
+        self.states[plaintiff] = plaintiffState;
+        totalAccusations = ++accusedState.accusations;
+        self.states[accused] = accusedState;
     }
 
     /**
      * @notice Responds to a complaint from a plaintiff, resolving it.
      * @param self The storage struct.
-     * @param plaintiff The plaintiff's FROST identifier.
-     * @param accused The accused's FROST identifier.
+     * @param plaintiff The plaintiff's address.
+     * @param accused The accused's address.
      * @dev When accused, a participant can resolve the complaint by taking an action, which is signaled by calling
      *      this function. This involves revealing the secret share that was sent to the plaintiff. This function
      *      decrements the complaint/accusation counters, marking the specific dispute as resolved.
      */
-    function respond(T storage self, FROST.Identifier plaintiff, FROST.Identifier accused) internal {
-        require(self.states[plaintiff].statuses[accused] == ComplaintStatus.SUBMITTED, ComplaintNotSubmitted());
-        self.states[plaintiff].statuses[accused] = ComplaintStatus.RESPONDED;
+    function respond(T storage self, address plaintiff, address accused) internal {
+        require(self.complaints[plaintiff][accused] == ComplaintStatus.SUBMITTED, NotComplaining());
+        self.complaints[plaintiff][accused] = ComplaintStatus.RESPONDED;
         self.states[plaintiff].complaints--;
         self.states[accused].accusations--;
     }
@@ -222,26 +211,26 @@ library FROSTParticipantMap {
     /**
      * @notice Confirms the Key Gen process for a participant, marking their successful completion.
      * @param self The storage struct.
-     * @param participant The participant's FROST identifier.
+     * @param participant The participant's address.
      * @dev A participant can only confirm if they have no outstanding complaints filed against others. This ensures
      *      all disputes are resolved before the group is finalized. Once confirmed, a participant can no longer file
      *      new complaints.
      */
-    function confirm(T storage self, FROST.Identifier participant) internal {
-        require(self.states[participant].complaints == 0, UnrespondedComplaintExists());
-        require(!self.states[participant].confirmed, ParticipantAlreadyFinalized());
-        self.states[participant].confirmed = true;
+    function confirm(T storage self, address participant) internal {
+        ParticipantState memory state = self.states[participant];
+        require(state.status == ParticipantStatus.REGISTERED, InvalidParticipant());
+        require(state.complaints == 0, UnrespondedComplaints());
+        state.status = ParticipantStatus.CONFIRMED;
+        self.states[participant] = state;
     }
 
     /**
-     * @notice Gets the participant's identifier.
+     * @notice Verifies that a participant is part of the map and confirmed.
      * @param self The storage struct.
      * @param participant The participant's address.
-     * @return identifier The participant's FROST identifier.
      */
-    function identifierOf(T storage self, address participant) internal view returns (FROST.Identifier identifier) {
-        identifier = self.identifiers[participant];
-        require(FROST.Identifier.unwrap(identifier) != 0, NotParticipating());
+    function verify(T storage self, address participant) internal view {
+        require(self.states[participant].status == ParticipantStatus.CONFIRMED, InvalidParticipant());
     }
 
     /**
@@ -256,22 +245,11 @@ library FROSTParticipantMap {
     /**
      * @notice Gets the participants verification share.
      * @param self The storage struct.
-     * @param identifier The participant's FROST identifier.
+     * @param participant The participant's address.
      * @return y The participant's public verification share.
      */
-    function getKey(T storage self, FROST.Identifier identifier) internal view returns (Secp256k1.Point memory y) {
-        y = self.keys[identifier];
-        require(y.x | y.y != 0, NotParticipating());
-    }
-
-    /**
-     * @notice Returns whether or not a participant is registered.
-     * @param self The storage struct.
-     * @param identifier The participant's FROST identifier.
-     * @return True if the participant is registered, false otherwise.
-     */
-    function isParticipating(T storage self, FROST.Identifier identifier) internal view returns (bool) {
-        Secp256k1.Point memory y = self.keys[identifier];
-        return y.x | y.y != 0;
+    function getKey(T storage self, address participant) internal view returns (Secp256k1.Point memory y) {
+        y = self.keys[participant];
+        require(y.x | y.y != 0, InvalidParticipant());
     }
 }
