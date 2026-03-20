97fb4d4f4a88d362241812a7f031ce89d9852dc7
Nicholas Rodrigues Lordello <nick@safe.dev>
2026-03-20 18:36:21 +0100

    Add Script For Detecting Unreliable RPCs
    This PR adds a script for detecting whether or not a configured RPC
    URL responds unreliably to `eth_getlogs` queries.
    
    The longer it runs, the more certainty you have that its reliable!

---
+++ validator/src/cmd/logs-reliability.ts
@@ -0,0 +1,75 @@
+import dotenv from "dotenv";
+import { type Chain, createPublicClient, extractChain, http } from "viem";
+import { supportedChains } from "../types/chains.js";
+import { validatorConfigSchema } from "../types/schemas.js";
+import { computeLogsBloom } from "../utils/bloom.js";
+import { BlockWatcher } from "../watcher/blocks.js";
+
+dotenv.config({ quiet: true });
+
+function chainBlockTime(chainId: number): number | undefined {
+	try {
+		const { blockTime } = extractChain({
+			chains: supportedChains as Chain[],
+			id: chainId,
+		});
+		return blockTime;
+	} catch {
+		return undefined;
+	}
+}
+
+const main = async (): Promise<void> => {
+	const config = validatorConfigSchema
+		.pick({
+			RPC_URL: true,
+			BLOCK_TIME_OVERRIDE: true,
+		})
+		.parse(process.env);
+
+	console.log(config);
+
+	const client = createPublicClient({
+		transport: http(config.RPC_URL),
+	});
+
+	const chainId = await client.getChainId();
+	const blockTime = config.BLOCK_TIME_OVERRIDE ?? chainBlockTime(chainId);
+	if (blockTime === undefined) {
+		throw new Error("must configure BLOCK_TIME_OVERRIDE");
+	}
+
+	console.log({
+		version: await client.request({ method: "web3_clientVersion" }),
+	});
+
+	const blocks = await BlockWatcher.create({
+		client,
+		lastIndexedBlock: null,
+		blockTime,
+		maxReorgDepth: 0,
+	});
+
+	while (true) {
+		const update = await blocks.next();
+		if (update.type !== "watcher_update_new_block") {
+			continue;
+		}
+
+		console.log(`saw block ${update.blockNumber}`);
+
+		const logs = await client.getLogs({
+			blockHash: update.blockHash,
+		});
+		if (update.logsBloom !== computeLogsBloom(logs)) {
+			console.log("=== DETECTED MISSING EVENTS! ===");
+			console.log("The connected RPC does not reliably query logs.");
+			break;
+		}
+	}
+};
+
+main().catch((err) => {
+	console.error(err);
+	process.exitCode = 1;
+});
