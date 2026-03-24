34621dcaf6ebdf4662fd014407da76cf1bdea126
Nicholas Rodrigues Lordello <n@lordello.net>
2026-03-24 07:35:07 +0100

    Fix MultiSend Decoding for Eth Transfers (#329)
    This PR fixes a subtle bug in the `multiSend` sub-transaction decoding
    logic that caused transactions to be falsely flagged as invalid. In
    particular, `viem`'s `slice` implementation does not allow slicing
    0-length chunks at the very end of some bytes. This happens when the
    last transaction in a `multiSend` is an Eth transfer (with `dataLength`
    set to `0` as the very last thing we read from the encoded transaction
    bytes), and was causing an otherwise valid transaction to be flagged as
    invalid.
    
    This PR fixes this, and moves some slicing logic around, adding some new
    unit tests to ensure things work as expected.

--- validator/src/consensus/verify/safeTx/checks/multisend.ts
+++ validator/src/consensus/verify/safeTx/checks/multisend.ts
@@ -5,7 +5,6 @@ import { buildFixedParamsCheck } from "./basic.js";
 import { TransactionCheckError } from "./errors.js";
 
 const MULTI_SEND = parseAbiItem("function multiSend(bytes transactions)");
-const TRANSACTION_FIXED_SIZE = 1 + 20 + 32 + 32;
 
 const decodeMultiSendFunctionData = ({ data }: Pick<SafeTransaction, "data">): Hex => {
 	try {
@@ -21,6 +20,19 @@ const decodeMultiSendFunctionData = ({ data }: Pick<SafeTransaction, "data">): H
 	}
 };
 
+const checkedSlice = (data: Hex, offset: number, len: number): Hex => {
+	// Viem doesn't like reading 0-length slices at the end of bytes, so handle
+	// that case manually.
+	if (len === 0 && offset <= size(data)) {
+		return "0x";
+	}
+	try {
+		return slice(data, offset, offset + len, { strict: true });
+	} catch {
+		throw new TransactionCheckError("invalid_multisend", "Invalid MultiSend transaction encoding");
+	}
+};
+
 const decodeMultiSend = (
 	{ chainId, safe, data, nonce }: Pick<SafeTransaction, "chainId" | "safe" | "data" | "nonce">,
 	options: MultiSendOptions,
@@ -28,15 +40,15 @@ const decodeMultiSend = (
 	const txs = decodeMultiSendFunctionData({ data });
 	const result: SafeTransaction[] = [];
 	let pointer = 0;
-	while (pointer + TRANSACTION_FIXED_SIZE <= size(txs)) {
+	while (pointer < size(txs)) {
 		// Read 1 byte for the operation as number
-		const operation = Number(slice(txs, pointer, pointer + 1));
+		const operation = Number(checkedSlice(txs, pointer, 1));
 		if (operation !== 0 && operation !== 1) {
 			throw new TransactionCheckError("invalid_multisend", `Invalid MultiSend operation ${operation}`);
 		}
 		pointer += 1;
 		// Read 20 bytes for to as an address
-		let to = getAddress(slice(txs, pointer, pointer + 20));
+		let to = getAddress(checkedSlice(txs, pointer, 20));
 		if (options.toZeroIsSelf === true && to === zeroAddress) {
 			// In recent versions of the Safe contracts (v1.5.0+), setting the `to` to be the zero address
 			// causes the contract to call itself.
@@ -44,13 +56,13 @@ const decodeMultiSend = (
 		}
 		pointer += 20;
 		// Read 32 bytes for the value as a bigint
-		const value = BigInt(slice(txs, pointer, pointer + 32));
+		const value = BigInt(checkedSlice(txs, pointer, 32));
 		pointer += 32;
 		// Read 32 bytes for the sub-data length as a number
-		const subDataLength = Number(slice(txs, pointer, pointer + 32));
+		const subDataLength = Number(checkedSlice(txs, pointer, 32));
 		pointer += 32;
 		// Read the sub-data bytes
-		const subData = slice(txs, pointer, pointer + subDataLength);
+		const subData = checkedSlice(txs, pointer, subDataLength);
 		pointer += subDataLength;
 		result.push({
 			chainId,
@@ -69,12 +81,7 @@ const decodeMultiSend = (
 			nonce,
 		});
 	}
-	// Check that the pointer is exactly at the end of the transactions. This is not the case if:
-	// - We reach a point where there are fewer than `TRANSACTION_FIXED_SIZE` bytes remaining, but
-	//   we did not reach the end of the transaction bytes.
-	if (pointer !== size(txs)) {
-		throw new TransactionCheckError("invalid_multisend", "Invalid MultiSend transaction encoding");
-	}
+
 	return result;
 };
 
