533d6a303e57713be00946ea9a979fe21be27bf0
Richard Meissner <msc.meissner@gmail.com>
2026-03-24 17:33:50 +0100

    Update defaults to beta config

--- explorer/src/lib/settings.ts
+++ explorer/src/lib/settings.ts
@@ -7,12 +7,12 @@ const STORAGE_KEY_SAFE_API_SETTINGS = "localStorage.safe_api_settings.object.v1"
 const STORAGE_KEY_SAFE_UI_SETTINGS = "localStorage.safe_ui_settings.object.v1";
 
 const DEFAULT_SETTINGS = {
-	consensus: "0x21c2B6C8473051fb6dED83fE6F0609e53747Cbb4" as Address,
+	consensus: "0x223624cBF099e5a8f8cD5aF22aFa424a1d1acEE9" as Address,
 	rpc: "https://1rpc.io/gnosis",
 	decoder: "https://calldata.swiss-knife.xyz/decoder?calldata=",
 	maxBlockRange: 10000,
 	validatorInfo:
-		"https://raw.githubusercontent.com/safe-fndn/safenet-beta-data/refs/heads/devnet/assets/validator-info.json",
+		"https://raw.githubusercontent.com/safe-fndn/safenet-beta-data/refs/heads/main/assets/validator-info.json",
 	refetchInterval: 10000,
 	blocksPerEpoch: 1440,
 	signingTimeout: 12,
