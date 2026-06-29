a433547326b1f9d2e3478f74ece694626a4dfb4b
Nicholas Rodrigues Lordello <nick@safe.dev>
2026-06-29 14:30:00 +0200

    Introduce Validator Rust Crate (#489)
    This PR just lays some initial ground work and creates a new crate for
    the Rust validator binary.
    
    ### Decisions and Tradeoffs
    
    - For now, the binary just parses CLI arguments and reads a
    configuration file. I decided to use configuration files instead of
    environment variables or command line arguments are they are (IMO)
    easier to deal with. In particular, in our infra services that are
    configured through files are generally easier to deal with (they do not
    involve `daemon-reload`-ing systemd unit files) and allow for more
    structured input (like arrays with nested objects, for example for the
    validator configuration). I chose TOML as I find it strikes a great
    balance between a simple format (unlike YAML) yet easy to edit and read
    (unlike JSON).
    - We setup observability to see logs, just to do some tiny amount of
    visible work.
    - We move all shared (or reasonably sharable) crates into workspace
    dependencies. This ensures we build one version/feature set for all our
    crates instead of having potentially multiple versions or crates (which
    can cause weird bugs or cryptic compiler errors). Note that a couple
    crates are intentionally not workspace dependencies, as they are the
    "implementation backend" choice for the shared crates:
    - `tracing-subscriber`: decides how tracing is recorded (for us, stdout)
    which should only be added as a dependency where the subscriber is
    initialized, and not where tracing is used
    - `metrics-exporter-prometheus`: similar argument to the tracing
    subscriber; this defines how metrics are hosted and is not required
    where metrics are used
    - Added a Git ignore rule for validator configuration. I included an
    extra glob-`*` in the pattern to make it easier for multiple
    configuration files (like `validator.dev.toml` and
    `validator.local.toml`) to live at the root of the repository (making it
    easy to run with `cargo run -qp validator -- --config-file
    validator.dev.toml` for example) without the configuration files being
    accidentally checked into version control.
    
    ### Testing
    
    ```
    $ cargo run -qp validator
    2026-06-29T10:24:22.236335Z  INFO safenet_core::observability: serving prometheus metrics and health endpoint metrics_addr=127.0.0.1:46049
    2026-06-29T10:24:22.236409Z DEBUG validator: validator configuration loaded config_file=validator.toml
    ```
    
    ---
    
    ## CLA signature
    
    With the submission of this Pull Request, I confirm that I have read and
    agree to the terms of the [Contributor License
    Agreement](https://docs.safefoundation.org/cla).

---
+++ crates/validator/src/config.rs
@@ -0,0 +1,30 @@
+use safenet_core::observability;
+use serde::Deserialize;
+use std::path::Path;
+use tokio::{fs, io};
+
+/// Error produced by the block watcher.
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// An IO error when interacting with the filesystem.
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    /// Error when parsing the configuration.
+    #[error(transparent)]
+    Parse(#[from] toml::de::Error),
+}
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(default)]
+pub struct Config {
+    pub observability: observability::Config,
+}
+
+impl Config {
+    /// Loads a configuration from a file.
+    pub async fn load(file: &Path) -> Result<Self, Error> {
+        let contents = fs::read_to_string(file).await?;
+        let config = toml::from_str(&contents)?;
+        Ok(config)
+    }
+}
