5c8fe3c9905890f2540c0ccc3fb95b4ecc70b8ae
Richard Meissner <richard@safe.dev>
2026-09-04 12:00:36 +0200

    [Part 4] Add handle oracle result method
    Add a method to handle oracle result events in preparation to have the
    sentinel be more contract event driven.
    
    Note: this method is not yet connected
    
    Design Decisions:
    - If in a state where a commit was performed, always handle this event,
      even if unexpected, to allow claiming of bonds.

--- crates/sentinel/src/bindings.rs
+++ crates/sentinel/src/bindings.rs
@@ -40,6 +40,7 @@ pub mod oracle {
                 string reason
             );
             event DisputeResolved(bytes32 indexed requestId, RequestState outcome, uint128 slashed, string context);
+            event OracleResult(bytes32 indexed requestId, address indexed sponsor, bytes result, bool approved);
             event Claimed(bytes32 indexed requestId, address indexed sentinel, uint96 bondReturn, uint96 feeReward);
 
             function commit(bytes32 requestId, bytes32 commitHash) external;
